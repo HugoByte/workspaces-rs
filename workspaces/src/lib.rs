5cbff668d5602e675c4e2fee06da5585f764bff7
Muhammed Irfan <muhammedirfan@hugobyte.com>
2022-02-06 00:19:11 +0530

    export `SecretKey`

--- workspaces/src/lib.rs
+++ workspaces/src/lib.rs
@@ -6,7 +6,7 @@ mod worker;
 pub mod prelude;
 
 pub use network::{Account, Contract, DevNetwork, Network};
-pub use types::{AccountId, BlockHeight, CryptoHash, InMemorySigner};
+pub use types::{AccountId, BlockHeight, CryptoHash, InMemorySigner, SecretKey};
 pub use worker::{
     mainnet, mainnet_archival, sandbox, testnet, with_mainnet, with_sandbox, with_testnet, Worker,
 };
