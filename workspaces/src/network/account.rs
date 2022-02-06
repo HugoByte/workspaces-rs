009e712891cf69797b3abde408ab1917863f4381
Muhammed Irfan <muhammedirfan@hugobyte.com>
2022-02-06 16:31:22 +0530

    enable public access for `InMemorySigner`

--- workspaces/src/network/account.rs
+++ workspaces/src/network/account.rs
@@ -30,7 +30,7 @@ impl Account {
         &self.id
     }
 
-    pub(crate) fn signer(&self) -> &InMemorySigner {
+    pub fn signer(&self) -> &InMemorySigner {
         &self.signer
     }
 
@@ -136,7 +136,7 @@ impl Contract {
         &self.account
     }
 
-    pub(crate) fn signer(&self) -> &InMemorySigner {
+    pub fn signer(&self) -> &InMemorySigner {
         self.account.signer()
     }
 
